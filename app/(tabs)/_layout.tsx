d0a52e2195b39d8b31f44da28e4c16c7a1d76004
Mozaik Ramadhan <142678727+Ozaimoza@users.noreply.github.com>
2025-08-25 17:46:54 +0700

    Normalize line endings

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,105 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Tabs } from "expo-router";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Image, ImageBackground, Text, View } from "react-native";
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+const Tabicon = ({ focused, icon, tittle }: any) => {
+  if (focused) {
+    return (
+      <ImageBackground
+        source={images.highlight}
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-6 justify-center items-center rounded-full overflow-hidden"
+      >
+        <Image source={icon} tintColor="#151312" className="size-5" />
+        <Text className="text-secondary text-base font-semibold ml-2">
+          {tittle}
+        </Text>
+      </ImageBackground>
+    );
+  } else {
+    return (
+      <View className="size-full justify-center items-center mt-4 rounded-full">
+        <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+      </View>
+    );
+  }
+};
 
+const _layout = () => {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0f0D23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 40,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0f0D23",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <Tabicon focused={focused} icon={icons.home} tittle="Home" />
+            </>
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="saved"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Saved",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <Tabicon focused={focused} icon={icons.save} tittle="Saved" />
+            </>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <Tabicon focused={focused} icon={icons.search} tittle="Search" />
+            </>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <Tabicon focused={focused} icon={icons.person} tittle="Profile" />
+            </>
+          ),
         }}
       />
     </Tabs>
   );
-}
+};
+
+export default _layout;
